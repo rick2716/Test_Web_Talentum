4b3bfd27ee7c6b9cead0d0097a73c55a8427959a
rick2716 <89090386+rick2716@users.noreply.github.com>
2024-08-12 11:16:49 -0500

    Modificación cards

--- test_web/src/Components/card.tsx
+++ test_web/src/Components/card.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
-import Link from 'next/link';
 
 export default function CityCard() {
     return (
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10">
             <div className="group relative bg-white rounded-lg">
+                <div className="absolute top-0 left-0 right-0 text-center">
+                    <h2 className="text-black text-2xl mt-6">Santiago de Cali</h2>
+                </div>
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-lg bg-gray-200 lg:aspect-none">
-                    <Image src="/Backgrounds/Cali.png" alt="Front of men&#039;s Basic Tee in black."
+                    <Image src="/Backgrounds/Cali.png" alt="Imagen Ciudad"
                         className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                         width={1000}
                         height={1000} />
@@ -14,7 +16,7 @@ export default function CityCard() {
                 <div className="mt-4 flex flex-col">
                     <div className="flex text-center py-2 border-b-2 justify-between" style={{ borderColor: 'rgba(0, 0, 0, 0.5)' }}>
                         <div className="flex flex-row">
-                            <Image src="/Icons/temp.png" width={30} height={30} alt="icono humedad" />
+                            <Image src="/Icons/temp.png" width={30} height={30} alt="icono temperatura" />
                             <h3 className="text-sm text-gray-700"> Temperatura </h3>
                         </div>
                         <p className="text-black ">5000</p>
@@ -28,7 +30,7 @@ export default function CityCard() {
                     </div>
                     <div className="flex text-center py-2 justify-between">
                         <div className="flex flex-row">
-                            <Image src="/Icons/wind.png" width={30} height={30} alt="icono humedad" />
+                            <Image src="/Icons/wind.png" width={30} height={30} alt="icono velocidad viento" />
                             <h3 className="text-sm text-gray-700"> Velocidad Viento </h3>
                         </div>
                         <p className="text-black ">5000</p>
--- test_web/src/app/Home/page.tsx
+++ test_web/src/app/Home/page.tsx
@@ -3,27 +3,22 @@ import CityCard from "@/Components/card";
 export default function Home() {
     return (
         <div className="home">
-            <div className="px-4 py-5">
-                <form className="space-y-6">
-                    <div>
-                        <label htmlFor="ciudad" className="block text-sm font-medium leading-6">Ciudad</label>
-                        <div className="mt-2">
-                            <input id="ciudad" name="ciudad" type="text" autoComplete="ciudad"
-                                required className="block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                        </div>
-                    </div>
-
-                    <div>
-                        <button type="submit" className="flex w-1/10 justify-center rounded-md bg-purple-400 bg-opacity-70 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500">
-                            Buscar
-                        </button>
-                    </div>
+            <div className="px-4 py-4">
+                <form className="flex w-full items-center justify-start space-x-2 px-1.5">
+                    <input id="ciudad" name="ciudad" type="text" autoComplete="ciudad" placeholder="Ciudad"
+                        required className="block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <button type="submit" className="rounded-md bg-purple-400 bg-opacity-70 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-purple-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500">
+                        Buscar
+                    </button>
                 </form>
-                <div className="flex justify-around space-x-10">
-                    <CityCard />
-                    <CityCard />
-                    <CityCard />
-                </div>
+            </div>
+            <div className="grid grid-cols-3 px-6 gap-10">
+                <CityCard />
+                <CityCard />
+                <CityCard />
+                <CityCard />
+                <CityCard />
+                <CityCard />
             </div>
         </div>
     );
